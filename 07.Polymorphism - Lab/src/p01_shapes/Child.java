09ac499b21ecd79b9c244e7b2aa5be04cf2210d7
AleksandarBoev <aleksandarboev95@gmail.com>
2018-06-22 10:06:53 +0300

    Added Plymorphism - Lab project

---
+++ 07.Polymorphism - Lab/src/p01_shapes/Child.java
@@ -0,0 +1,7 @@
+package p01_shapes;
+
+public class Child extends Parent {
+    public Child(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
---
+++ 07.Polymorphism - Lab/src/p01_shapes/Parent.java
@@ -0,0 +1,11 @@
+package p01_shapes;
+
+public abstract class Parent {
+    protected String firstName;
+    protected String lastName;
+
+    public Parent(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
